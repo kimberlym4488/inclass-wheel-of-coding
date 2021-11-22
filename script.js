02cb054ff34f85f77b1d275c21e0d2b3c25a7996
SamusMoran <kimberlym4488@comcast.net>
2021-11-22 13:49:58 -0800

    This is a disaster, I'm so sorry!

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ var startButton = document.querySelector(".start-button");
 var timerCountEl = document.querySelector(".timer-count");
 var secondsLeft = 10;
 var wordBlank = document.querySelector(".word-blanks")
-var answer = 'JavaScript'.split('');
+var answer = "";
 var unsolved = ["_","_","_","_","_","_", "_","_","_","_"];
 var playing = false;
 var wins = 0;
-var losses = 0
+var losses = 0;
 
 
-wordBlank.textContent = unsolved.toString().replaceAll(",", " ");
+//wordBlank.textContent = unsolved.toString().replace(",", " ");
 
-console.log(timerCountEl)
 //Start Button should decrement timer.
 
-startButton.addEventListener("click", countDown);
+startButton.addEventListener("click", countDown)
+var secondsLeft = 10;
     //now we want the timer to start
     
 function countDown() {
-    console.log("")
     playing = true;
     var timerInterval = setInterval(function() {
         secondsLeft--;
-        timerCountEl.textContent = secondsLeft;
+        timerCountEl.textContent = parseInt(secondsLeft);
         //When timer ends, the ability to type will stop.
         if(secondsLeft === 0) {
-            playing = false;
-            clearInterval(timerInterval);//stops the timer interval button and returns the function/exits the function started by pressing the start button.
-            console.log(secondsLeft)
-            return;
+          clearInterval(timerInterval);
+          console.log(timerCountEl);
+          console.log(secondsLeft);
+          !playing;
+             //stops the timer interval button and returns the function/exits the function started by pressing the start button.
       //WILL call FUNCTION here for the you lost or you win and to add to wins/losses.
         }
     }, 1000);
@@ -36,14 +36,17 @@ function countDown() {
 //Some letters should be pre-populated
     //User should be able to type in letters to fill in the word.
     
-    document.addEventListener("keydown", function(e){
-        var letter = e.key;
+  if (playing && (secondsLeft > 0)) {
+    document.addEventListener("keydown", function(event) {
         console.log(letter);
+        var letter = event.key;
         if(answer.includes(letter)){
-            unsolved[answer.indexOf(letter)] = letter;
+            unsolved[answer.indexOf(letter)] = letter.toLowerCase();
+            console.log(letter);
         }
-    }
-    )
+      })
+  }
+
     /*If the word is filled in, the user will get a win in the win column.
     If the word is not filled in, the user will get a loss in the loss column. 
         These two items will remain when the user presses start again.
