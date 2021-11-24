c12f5033c7a0e82fb8bdfd8c70fc086183f58810
SamusMoran <kimberlym4488@comcast.net>
2021-11-23 19:35:12 -0800

    Updates for Tues evening

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 var startButton = document.querySelector(".start-button");
 var timerCountEl = document.querySelector(".timer-count");
 var secondsLeft = 5;
-var wordBlank = document.querySelector(".word-blanks")
-var answer = 'JavaScript'.split('');
+var wordBlank = document.querySelector(".word-blanks");
+var answer = ["J","A","V","A","S","C","R","I","P","T"];
 var unsolved = ["J","_","v","_","S","_","_","_","p","_"];
 var playing = false;
 var wins = 0;
 var losses = 0
 
+wordBlank.textContent = answer.toString().replaceAll(",", " ");
+console.log(wordBlank);
 wordBlank.textContent = unsolved.toString().replaceAll(",", " ");
+
 
 startButton.addEventListener("click", countDown);
 //Start Button should decrement timer.
-
 
 function countDown() {
   secondsLeft = 5;
@@ -20,6 +22,7 @@ function countDown() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timerCountEl.textContent = secondsLeft;
+    playingNow();
     //now we want the timer to start
 
         if(secondsLeft === 0) {
@@ -28,19 +31,22 @@ function countDown() {
             console.log(secondsLeft);
         }
         //now we want the timer to stop, and start over at 5 when the user starts again.
-
-//WILL call FUNCTION here for the you lost or you win and to add to wins/losses.
-    }, 1000);
-}
-
-//User should be able to type in letters to fill in the word.
-document.addEventListener("keydown", function(e){
-    var letter = e.key;
-    console.log(letter);
-    if(answer.includes(letter)){
-        unsolved[answer.indexOf(letter)] = letter;
-        console.log(letter);
+        
+        //WILL call FUNCTION here for the you lost or you win and to add to wins/losses.
+      }, 1000);
     }
-  }
-)
+    
+    function playingNow() {
+    document.addEventListener("keydown", function(e){
+        var letter = e.key;
+        if(answer.includes(letter)){
+            unsolved[answer.indexOf(letter)] = letter;
+            console.log(letter);
+    
+        }
+      }
+    )
+    }
+  
+//User should be able to type in letters to fill in the word.
 
